d44828a1b8cc4935346da5b1246bd13038800f15
zhangbz <zhangbz.happyhacking@gmail.com>
2015-01-25 23:32:54 +0800

    bottom_bar

--- src/com/zhangbz/pals/activity/LoginActivity.java
+++ src/com/zhangbz/pals/activity/LoginActivity.java
@@ -3,12 +3,27 @@ package com.zhangbz.pals.activity;
 import com.zhangbz.pals.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
+	private Button bt_new_user;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login);
+		setContentView(R.layout.activity_login);
+		bt_new_user = (Button) findViewById(R.id.bt_new_user);
+		bt_new_user.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
